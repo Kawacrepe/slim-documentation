a725206303905270cd77eff5a3020a6b069c749b
Kawacrepe <vincent.dhennin@viacesi.fr>
2020-06-11 21:05:11 +0200

    I can serve static html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
+const { join } = require('path');
 const polka = require('polka');
 const PORT = process.env.PORT;
+const dir = join(__dirname, '../public');
+const serve = require('serve-static')(dir);
+
 const buildDoc = require('./buildDoc');
 
 polka()
+  .use(serve)
   .get('/build', (req, res) => {
     buildDoc();
     res.end('Je build ici');
-  })
-  .get('/', (req, res) => {
-    res.end('serve');
   })
   .listen(PORT, err => {
     if(err) throw err;
